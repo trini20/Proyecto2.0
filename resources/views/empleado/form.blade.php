845d3a5794c40aed6c3e184b4a7ca563e5252563
trini20 <jackelin.guerrero6013@alumnos.udg.mx>
2023-05-26 19:41:32 -0600

    nuevoproyecto

---
+++ resources/views/empleado/form.blade.php
@@ -0,0 +1,53 @@
+
+<h1>{{ $modo }} empleado </h1>
+
+@if(count($errors)>0)
+
+    <div class="alert alert-danger" role="alert">
+<ul>
+    @foreach($errors->all() as $error)
+       <li> {{ $error }} </li>
+    @endforeach
+</ul>
+    </div>
+
+    
+@endif
+
+<div class="form-group">
+<label for="Nombre"> Nombre </label>
+<input type="text" class="form-control" name="Nombre" 
+value="{{ isset($empleado->Nombre)?$empleado->Nombre:old('Nombre') }}" id="Nombre">
+</div>
+
+<div class="form-group">
+<label for="ApellidoPaterno"> Apellido Paterno </label>
+<input type="text" class="form-control" name="ApellidoPaterno" 
+value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:old('ApellidoPaterno') }}" id="ApellidoPaterno">
+</div>
+
+<div class="form-group">
+<label for="ApellidoMaterno"> Apellido Materno </label>
+<input type="text" class="form-control" name="ApellidoMaterno" 
+value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:old('ApellidoMaterno') }}" id="ApellidoMaterno">
+</div>
+
+<div class="form-group">
+<label for="Telefono"> Telefono </label>
+<input type="text" class="form-control" name="Telefono" 
+value="{{ isset($empleado->Telefono)?$empleado->Telefono:old('Telefono') }}" id="Telefono">
+</div>
+
+<div class="form-group">
+<label for="Foto">  </label>
+@if(isset($empleado->Foto))
+<img class="img-thumbnail img-fluid" src="{{ asset('storage'.'/'.$empleado->Foto ) }}" width="100" alt="">
+@endif
+<input type="file" class="form-control" name="Foto" value="" id="Foto">
+</div>
+
+<input class="btn btn-success" type="submit" value="{{ $modo }} datos">
+
+<a class="btn btn-primary" href="{{ url('empleado/') }}">Regresar</a>
+
+<br>
